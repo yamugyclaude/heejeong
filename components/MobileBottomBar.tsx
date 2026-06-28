541869d50f39088823b9f2e8756fa1b456c10c3c
yamugyclaude <yamugyclaude@gmail.com>
2026-06-28 23:30:07 +0900

    feat: 전화번호 제거

--- components/MobileBottomBar.tsx
+++ components/MobileBottomBar.tsx
@@ -1,14 +1,3 @@
-'use client'
-
-export default function MobileBottomBar({ phone }: { phone: string }) {
-  return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 flex md:hidden">
-      <a
-        href={`tel:${phone}`}
-        className="flex-1 bg-[#1E3A5F] text-white py-4 text-center font-bold text-sm"
-      >
-        📞 {phone} 전화상담
-      </a>
-    </div>
-  )
+export default function MobileBottomBar() {
+  return <div />
 }
